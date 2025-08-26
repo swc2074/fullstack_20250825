01121ed913d1986eaf72365845bf0e025aa7c31f
sangwook choi <gforce74@naver.com>
2025-08-26 17:44:37 +0900

    day002 git 정리

---
+++ track003_java/java2025/src/com/company/java001/A000.java
@@ -0,0 +1,13 @@
+package com.company.java001; // 1. 부품의 위치
+
+// 주석, 설명
+public class A000 { // 2. 어디에서든지 접근가능, 부품객체, 부품이름(클래스 이름)
+	public                  static           void main(String[] args) {
+		// 3. 어디에서든지 접근, 바로사용가능 메모리,  void main(String[] args) 전원버튼 이름
+		 System.out.println("Hello Java");
+		 // 4. 운영체제부품객체. 출력 (cmd) , println("하고싶은말") 출력
+	}
+
+} // end class
+
+// ctrl + f11 (실행)
---
+++ track003_java/java2025/src/com/company/java001_ex/A000_ex.java
@@ -0,0 +1,7 @@
+package com.company.java001_ex;
+
+public class A000_ex {
+ public static void main(String[] args) {
+	 System.out.println("한번작성하면 어디서든 실행된다.");
+ }
+}
